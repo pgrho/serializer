1d1dda36d1e07daa21c050b0e28ecee1c9c346cb
pgrho <shipwreck@live.jp>
2022-07-13 09:34:13 +0900

    Initial Commit

---
+++ src/Shipwreck.Serializers/EntryType.cs
@@ -0,0 +1,30 @@
+﻿namespace Shipwreck.Serializers;
+
+public enum EntryType
+{
+    StartObject,
+    EndObject,
+    StartArray,
+    EndArray,
+
+    Property,
+
+    Null,
+    Boolean,
+    String,
+
+    SByte,
+    Int16,
+    Int32,
+    Int64,
+    Byte,
+    UInt16,
+    UInt32,
+    UInt64,
+
+    Single,
+    Double,
+
+    DateTime,
+    DateTimeOffset,
+}
---
+++ src/Shipwreck.Serializers/SerializationReader.cs
@@ -0,0 +1,10 @@
+﻿namespace Shipwreck.Serializers;
+
+public abstract class SerializationReader
+{
+    public abstract EntryType Type { get; }
+
+    public abstract object Value { get; }
+
+    public abstract bool Read();
+}
